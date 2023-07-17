22a66881f2396464ba7bb2b4a942dc66d0a7097b
trungquy2k1 <trungquy1905@gmail.com>
2023-07-17 09:45:25 +0700

    trang chitiet va trang dat mua

---
+++ App.tsx
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {Chitietsp} from './Screen/Chitietsp';
+import {Dathang} from './Screen/DathangTsx';
+export default Dathang;
